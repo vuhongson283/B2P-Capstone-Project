91f76fe4828750b64989f5416e696d48d6ddaf3e
vuhongson283 <vuson020803@gmail.com>
2025-06-20 12:34:08 +0700

    update b2p_frontend

---
+++ b2p_frontend/src/store/reducer/rootReducer.js
@@ -0,0 +1,13 @@
+﻿import { combineReducers } from "redux";
+
+const rootReducer = combineReducers({
+  // Add your reducers here
+  // Example: user: userReducer, products: productReducer
+  // user: userReducer,
+  // products: productReducer,
+  // cart: cartReducer,
+  // orders: orderReducer,
+  // etc.
+});
+
+export default rootReducer;
