c8731aef4f6e547665716d53956d9757db995a65
jendly365 <trong1met8@gmail.com>
2025-06-28 15:29:36 +0700

    commit for checkout

---
+++ B2P_API/B2P_API/Models/Slider.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace B2P_API.Models;
+
+public partial class Slider
+{
+    public int SlideId { get; set; }
+
+    public string? SlideDescription { get; set; }
+
+    public string? SlideUrl { get; set; }
+
+    public virtual ICollection<Image> Images { get; set; } = new List<Image>();
+}
