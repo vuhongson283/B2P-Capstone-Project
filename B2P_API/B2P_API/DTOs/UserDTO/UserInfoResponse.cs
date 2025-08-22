7a83fe3b094290d4bc84e806d8b561a11c47c2f8
CDQuan88 <duyquan7b@gmail.com>
2025-08-23 00:07:50 +0700

    no message

--- B2P_API/B2P_API/DTOs/UserDTO/UserInfoResponse.cs
+++ B2P_API/B2P_API/DTOs/UserDTO/UserInfoResponse.cs
@@ -5,14 +5,14 @@ namespace B2P_API.DTOs.UserDTO;
 public class UserInfoResponse
 {
     public string FullName { get; set; } = null!;
-    public string Email { get; set; } = null!;
-    public string Phone { get; set; } = null!;
+    public string? Email { get; set; } = null!;
+    public string? Phone { get; set; } = null!;
     public bool? IsMale { get; set; }
     public string? Address { get; set; }
     public DateOnly? Dob { get; set; }
     public DateTime? CreateAt { get; set; }
     public string? StatusDescription { get; set; }
-    public int ImageId { get; set; }
-    public string ImageUrl { get; set; } = null!;
+    public int? ImageId { get; set; }
+    public string? ImageUrl { get; set; } = null!;
 
 }
